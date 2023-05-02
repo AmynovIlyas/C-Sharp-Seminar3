f13cea769a0c3eab79f7f7c9b2f6266c0df4fe96
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-05-03 00:25:17 +0300

    Solution of the task 17 about Coordinats

---
+++ task_17/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X != 0 и Y != 0 и выдаёт номер четверти плоскости, в которой находится эта точка
+
+int valueX = Prompt("Введите координату Х: ");
+int valueY = Prompt("Введите координату Y: ");
+FindQuarterPlane(valueX, valueY);
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int coordNumber = Convert.ToInt32(Console.ReadLine());
+    return coordNumber;
+}
+
+void FindQuarterPlane(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("1 четверть");
+    }
+    if (x < 0 && y > 0)
+    {
+        Console.WriteLine("2 четверть");
+    }
+    if (x < 0 && y < 0)
+    {
+        Console.WriteLine("3 четверть");
+    }
+    if (x > 0 && y < 0)
+    {
+        Console.WriteLine("4 четверть");
+    }
+}
