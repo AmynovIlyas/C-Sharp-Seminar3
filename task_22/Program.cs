b570407b03f1d56f13e08cfbc56c44232c5d3a1b
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-05-04 23:03:43 +0300

    Solution of the task 22 about SquareDigitsFrom1ToN

--- task_22/Program.cs
+++ task_22/Program.cs
@@ -3,3 +3,26 @@
 // 5 --> 1, 4, 9, 16, 25
 // 2 --> 1, 4
 
+int N = Prompt("Введите число: ");
+Console.Write($"{N} --> ");
+int finalN = Math.Abs(N);
+SquareDigit(finalN);
+
+void SquareDigit(int number)
+{
+    int i = 1;
+    while (i < number) 
+    {
+        int square = Convert.ToInt32(Math.Pow(i, 2));
+        Console.Write($"{square}, ");
+        i++;
+    }
+    Console.Write(Math.Pow(i, 2));
+}
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
